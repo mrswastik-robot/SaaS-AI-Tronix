d69d4f2a9a75a56ad4908538e0dc757fa9ec7b16
swastik <swastikpatel29@gmail.com>
2023-07-25 00:46:49 +0530

    sidebar created for all screens

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Button } from './ui/button';
-import { Menu } from 'lucide-react';
+// import { Button } from './ui/button';
+// import { Menu } from 'lucide-react';
 import { UserButton } from '@clerk/nextjs';
+import MobileSidebar from './mobile-sidebar';
 
 const Navbar = () => {
   return (
     <div className=' flex items-center p-4'>
-        <Button variant="ghost" size="icon" className=' md:hidden'>
+        {/* <Button variant="ghost" size="icon" className=' md:hidden'>
             <Menu/>
-        </Button>
+        </Button> */}
+
+        <MobileSidebar/>
+
 
         <div className=' flex w-full justify-end'>
             <UserButton afterSignOutUrl='/'/>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,9 +5,61 @@ import Image from "next/image";
 import Link from "next/link";
 import {cn} from "@/lib/utils";
 
+import {usePathname} from 'next/navigation';
+
 const montserrat = Montserrat({ weight: "600",subsets: ["latin"] });
 
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+
+const routes = [
+    {
+      label: 'Dashboard',
+      icon: LayoutDashboard,
+      href: '/dashboard',
+      color: "text-sky-500"
+    },
+    {
+      label: 'Conversation',
+      icon: MessageSquare,
+      href: '/conversation',
+      color: "text-violet-500",
+    },
+    {
+      label: 'Image Generation',
+      icon: ImageIcon,
+      color: "text-pink-700",
+      href: '/image',
+    },
+    {
+      label: 'Video Generation',
+      icon: VideoIcon,
+      color: "text-orange-700",
+      href: '/video',
+    },
+    {
+      label: 'Music Generation',
+      icon: Music,
+      color: "text-emerald-500",
+      href: '/music',
+    },
+    {
+      label: 'Code Generation',
+      icon: Code,
+      color: "text-green-700",
+      href: '/code',
+    },
+    {
+      label: 'Settings',
+      icon: Settings,
+      href: '/settings',
+    },
+  ];
+
 const Sidebar = () => {
+
+    const pathname = usePathname();
+
+
   return (
     <div className=" space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
         <div className=" px-3 py-2 flex-1">
@@ -22,6 +74,22 @@ const Sidebar = () => {
                 </h1>
 
             </Link>
+
+            <div className=" space-y-1">
+                {routes.map((route) => (
+                    <Link
+                    href={route.href}
+                    key={route.href}
+                    className={cn("text-base group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                    pathname === route.href ? "text-white bg-white/10" : "text-zinc-400" )}>
+                        <div className=" flex items-center flex-1">
+                            <route.icon className={ cn(" h-5 w-5 mr-3 " , route.color) }/>
+                            {route.label}
+                        </div>
+                    </Link>
+                ))}
+
+            </div>
 
         </div>
 
