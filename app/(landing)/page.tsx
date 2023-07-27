952f42fef8f4c0bc26b704a0f0ba6eb23e6f96f1
swastik <swastikpatel29@gmail.com>
2023-07-27 23:36:51 +0530

    landingPage setup done , moving to hero section.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,24 +2,15 @@
 import {Button} from '@/components/ui/button';
 import Link from 'next/link';
 
+import {LandingNavbar} from '@/components/landing-navbar';
+import {LandingHero} from '@/components/landing-hero';
+
 const LandingPage = () => {
     return (
-        <div>
+        <div className=' h-full'>
+            <LandingNavbar />
+            <LandingHero/>
 
-            <Link href='/sign-in'>
-                <Button>
-                    Login
-                </Button>
-            </Link>
-
-            <Link href='/sign-up'>
-                <Button>
-                    Register
-                </Button>
-            </Link>
-
-
-    
         </div>
     );
     }
---
+++ components/landing-hero.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LandingHero = () => {
+  return (
+    <div>LandingHero</div>
+  )
+}
